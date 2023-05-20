c553ac1a986ee13010dd1f39b521f5bda1579582
Anmorhy <anmorhy@gmail.com>
2023-05-20 14:20:31 -0300

    ✨ node

--- Ganhando Produtividade Angular/02_TypeScript para Angular/01_Inicio/caseJS.js
+++
@@ -1,9 +0,0 @@
-function ligar(heroi){
-    console.log("ligando para :"+heroi.telefone);
-}
-
-ligar({
-    nome: "Steve Rogers",
-    vulgo: "Capitao America",
-    telefone: "123456789",
-})
--- Ganhando Produtividade Angular/02_TypeScript para Angular/src/index.ts
+++ Ganhando Produtividade Angular/02_TypeScript para Angular/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello");
+type heroi = {
+    name: string;
+    vulgo: string;
+}
+function printaObjetos(pessoa: heroi){
+    console.log(pessoa);
+}
+printaObjetos({
+    name: "Bruce Wayne",
+    vulgo: "Batman"
+});
